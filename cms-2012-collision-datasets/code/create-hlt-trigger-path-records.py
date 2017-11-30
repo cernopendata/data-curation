b8288974d1264ed617b4b56fee7a01b8802ffcc6
Tibor Simko <tibor.simko@cern.ch>
2017-11-30 20:58:57 +0100

    HLT trigger path: porting to COD JSON
    Signed-off-by: Tibor Simko <tibor.simko@cern.ch>

--- cms-2012-collision-datasets/code/create-hlt-trigger-path-records.py
+++ cms-2012-collision-datasets/code/create-hlt-trigger-path-records.py
@@ -5,6 +5,8 @@
 Create CMS Trigger Information records.
 """
 
+import json
+
 LINK_INFO = {}
 # read LINK_INFO dictionary created by a friendly program ahead of this one:
 exec(open('./outputs/hlt_config_files_link_info.py', 'r').read())
@@ -13,17 +15,6 @@ RECID_START = 6200
 
 RECORD_TEMPLATE = """\
   <record>
-    <controlfield tag="001">%(recid)s</controlfield>
-    <datafield tag="110" ind1=" " ind2=" ">
-        <subfield code="a">CMS collaboration</subfield>
-    </datafield>
-    <datafield tag="245" ind1=" " ind2=" ">
-        <subfield code="a">High-Level Trigger path information %(title)s</subfield>
-    </datafield>
-    <datafield tag="260" ind1=" " ind2=" ">
-        <subfield code="b">CERN Open Data Portal</subfield>
-        <subfield code="c">2016</subfield>
-    </datafield>
     <datafield tag="264" ind1=" " ind2="0">
         <subfield code="c">2011</subfield>
     </datafield>
@@ -34,16 +25,9 @@ RECORD_TEMPLATE = """\
       <subfield code="a">CERN-LHC</subfield>
       <subfield code="e">CMS</subfield>
     </datafield>
-    <datafield tag="942" ind1=" " ind2=" ">
-        <subfield code="e">7TeV</subfield>
-    </datafield>
     <datafield tag="964" ind1=" " ind2="0">
       <subfield code="c">Run2011A</subfield>
     </datafield>
-    <datafield tag="980" ind1=" " ind2=" ">
-        <subfield code="a">CMS-Trigger-Information</subfield>
-    </datafield>
-    <datafield tag="980" ind1=" " ind2=" ">
         <subfield code="b">Research</subfield>
     </datafield>
    </record>"""
@@ -60,33 +44,72 @@ def expand_links(atext):
 
 def create_rich_abstract(title, info):
     """Create rich abstract out of info items."""
-    out = '<![CDATA[ '
+    out = ' '
     out += '<p>High-Level Trigger path information %s.</p>' % title
     if info:
         out += '<blockquote>'
         for item in info:
             out += '<p>' + expand_links(item) + '</p>'
         out += '</blockquote>'
-    out += '<p>See also the full list of triggers for CMS 2011 open data:'
-    out += '<a href="/record/1700">Trigger information for 2011 CMS open data</a></p>'
-    return out + ']]>'
+    out += '<p>See also the full list of triggers for CMS 2012 open data:'
+    out += '<a href="/record/1701">Trigger information for 2012 CMS open data</a></p>'
+    return out + ''
 
 
 def main():
     """Do the main job."""
 
+    year_created = '2012'
+    year_published = '2017'
+    run_period = '2012RunB-2012RunC'
+
+    records = []
     recid = RECID_START
     title = None
     info = []
     for line in open('./inputs/hlt-trigger-path.txt', 'r').readlines():
         if line.startswith('Path'):
             if title:
-                print(RECORD_TEMPLATE %
-                      {'recid': recid,
-                       'title': title,
-                       'info': create_rich_abstract(title, info)})
+
+                rec = {}
+
+                rec['abstract'] = {}
+                rec['abstract']['description'] = create_rich_abstract(title, info)
+
+                rec['accelerator'] = "CERN-LHC"
+
+                rec['collaboration'] = {}
+                rec['collaboration']['name'] = 'CMS collaboration'
+                rec['collaboration']['recid'] = '451'
+
+                rec['collections'] = ['CMS-Trigger-Information', ]
+
+                rec['collision_information'] = {}
+                rec['collision_information']['energy'] = '8TeV'
+                rec['collision_information']['type'] = 'pp'
+
+                rec['date_created'] = year_created
+                rec['date_published'] = year_published
+
+                rec['experiment'] = 'CMS'
+
+                rec['publisher'] = 'CERN Open Data Portal'
+
+                rec['recid'] = str(recid)
+
+                rec['run_period'] = run_period
+
+                rec['title'] = 'High-Level Trigger path information ' + title
+
+                rec['type'] = {}
+                rec['type']['primary'] = 'Supplementaries'
+                rec['type']['secondary'] = ['Trigger', ]
+
+                records.append(rec)
+
                 recid += 1
                 info = []
+
             title = line.split(' ', 1)[1].strip()
             if title.endswith(':'):
                 title = title[:-1]
@@ -94,6 +117,8 @@ def main():
             info.append(line[2:].strip())
         else:
             pass
+
+    print(json.dumps(records, indent=2, sort_keys=True))
 
 
 if __name__ == '__main__':
