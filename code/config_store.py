d275c70bf47443c52684bbcdd73718b1b53bce20
Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>
2018-10-22 15:40:00 +0200

    add skeleton for get_conf_files

---
+++ code/config_store.py
@@ -0,0 +1,8 @@
+import os
+import subprocess
+
+
+def main(das_dir="./inputs/das-json-store",
+         conf_dir="./inputs/config-store",
+         datasets=[]):
+    print(conf_dir)
--- code/interface.py
+++ code/interface.py
@@ -25,9 +25,16 @@ from utils import get_datasets_from_dir
 @click.option('--das-dir', default='./inputs/das-json-store',
               show_default=True,
               help='Output directory for the DAS metadata')
+@click.option('--get-conf-files/--no-get-conf-files',
+              default=False, show_default=True,
+              help='Get configuration files for the datasets')
+@click.option('--conf-dir', default='./inputs/config-store',
+              show_default=True,
+              help='Output directory for the configuration files')
 def main(dataset_list, output_dir,
          create_eos_indexes, eos_dir,
-         create_das_json_store, das_dir):
+         create_das_json_store, das_dir,
+         get_conf_files, conf_dir):
     """
     Interface for manipulation of dataset records for OpenData portal.
 
@@ -49,6 +56,10 @@ def main(dataset_list, output_dir,
 
         $ voms-proxy-init -voms cms -rfc
         $ python ./code/interface.py --create-das-json-store DATASET_LIST
+
+    setp 3) get the config files
+
+        $ voms-proxy-init -voms cms -rfc
     """
     datasets = get_datasets_from_dir(dataset_list)
 
@@ -72,6 +83,17 @@ def main(dataset_list, output_dir,
         else:
             import das_json_store
             das_json_store.main(das_dir, eos_dir, datasets)
+
+    if get_conf_files:
+        # check if user has voms-proxy
+        proxyinfo = subprocess.run("voms-proxy-info", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        if proxyinfo.returncode != 0:
+            print("Error in VOMS proxy.")
+            print('Did you forget to "voms-proxy-init -voms cms -rfc"?')
+        else:
+            import config_store
+            config_store.main(das_dir, conf_dir, datasets)
+
 
 if __name__ == '__main__':
     main()
