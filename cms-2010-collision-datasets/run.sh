655f74642ffa8806a7ffed15f21a557a5ed03bd7
Tibor Simko <tibor.simko@cern.ch>
2019-07-03 14:54:36 +0200

    cms-2010-collision-datasets: Commissioning10
    Signed-off-by: Tibor Simko <tibor.simko@cern.ch>

--- cms-2010-collision-datasets/run.sh
+++ cms-2010-collision-datasets/run.sh
@@ -3,6 +3,10 @@
 # Create EOS file indexes for Run2010A datasets:
 # (we can reuse 2012 code)
  
-(cd inputs; ln -sf ./cms-Run2010A-collision-datasets.txt ./cms-2012-collision-datasets.txt)
+#(cd inputs; ln -sf ./cms-Run2010A-collision-datasets.txt ./cms-2012-collision-datasets.txt)
+#python ../cms-2012-collision-datasets/code/create_eos_file_indexes.py
+#rm -f ./inputs/cms-2012-collision-datasets.txt
+
+(cd inputs; ln -sf ./cms-Commissioning10-collision-datasets.txt ./cms-2012-collision-datasets.txt)
 python ../cms-2012-collision-datasets/code/create_eos_file_indexes.py
 rm -f ./inputs/cms-2012-collision-datasets.txt
