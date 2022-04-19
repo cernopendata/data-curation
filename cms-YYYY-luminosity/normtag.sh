18fe6915c6f85e793e196a45ca2d6e23cf2d679a
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2022-04-19 09:13:12 +0200

    remove last line comma

--- cms-YYYY-luminosity/normtag.sh
+++ cms-YYYY-luminosity/normtag.sh
@@ -11,19 +11,26 @@ runmax=$(echo "$cert" | jq -c -r '. | keys[]' | tail -1)
 
 tags=$(cat normtag_PHYSICS.json)
 
+arr=()
 echo "["
 for row in $(echo "${tags}" | jq -c '.[]'); do
    run=$(echo "${row}" | jq -c -r '.[1] | keys[]')
-   if [ "$run" -ge "$runmin" ] && [ "$run" -lt "$runmax" ]
+   if [ "$run" -ge "$runmin" ] && [ "$run" -le "$runmax" ]
    then
      myrow=$(echo "${row}" | jq -c -r) 
-     echo "$myrow"","
-   elif [ "$run" -eq "$runmax" ]
-   then
-     echo "${row}" | jq -c -r
+     arr+=("$myrow"",")
    elif [ "$run" -gt "$runmax" ]
    then
      break
    fi
 done
+
+# remove the comma after the last element
+arr[-1]=${arr[-1]%?}
+
+for i in "${arr[@]}"
+do
+   echo "$i"
+done
+
 echo "]"
