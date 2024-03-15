2dd979d60424999aa1161301d9f7be6e7e8f62a4
Richard Hildebrandt <richard.hildebrandt@mpp.mpg.de>
2024-03-15 13:05:17 +0100

    Reformat record creation file

--- jade-2023-raw-datasets/code/create_cn_records.py
+++ jade-2023-raw-datasets/code/create_cn_records.py
@@ -3,15 +3,17 @@ import numpy as np
 import time
 import regex as re
 
-with open('ComputingNotes.txt', 'r') as f:
-    datalist = f.read().split('\n\n')
+with open("ComputingNotes.txt", "r") as f:
+    datalist = f.read().split("\n\n")
 namelist = datalist[0:-3]
 authorlist = datalist[-3]
-fileinfo = datalist[-1].split('\n')
-authorlist = re.split(r'",\s*"', authorlist.strip('[]'))
-datelist = datalist[-2].strip('[]').split(", ")
-namelist, authorlist, datelist = np.array(sorted(list(zip(namelist, authorlist, datelist)))).T
-for item, i in zip(datelist, np.arange(0, len(datelist))):  #formatting the dates
+fileinfo = datalist[-1].split("\n")
+authorlist = re.split(r'",\s*"', authorlist.strip("[]"))
+datelist = datalist[-2].strip("[]").split(", ")
+namelist, authorlist, datelist = np.array(
+    sorted(list(zip(namelist, authorlist, datelist)))
+).T
+for item, i in zip(datelist, np.arange(0, len(datelist))):  # formatting the dates
     try:
         datelist[i] = item[0:2] + " " + item[2] + item[3]
         datelist[i] = datelist[i] + " " + item[4] + item[5]
@@ -19,40 +21,44 @@ for item, i in zip(datelist, np.arange(0, len(datelist))):  #formatting the date
     except IndexError:
         if len(datelist[i]) == 5:
             datelist[i] = time.strftime("%b %Y", time.strptime(datelist[i], "%m %y"))
-        else: pass
+        else:
+            pass
 
 cndict = []
 recid = 26150
 for name, authors, file, date in zip(namelist, authorlist, fileinfo, datelist):
     path, size, checksum = re.findall(r"=(\S*)", file)
     fields = list(filter(None, re.split(r"^(Note.\S*)\s", name)))
-    entry = {'abstract': {'description': fields[1]},
-    'accelerator': 'DESY-PETRA',
-    'collaboration': {'name': 'JADE collaboration', 'recid': "26050"},
-    'collections': ['JADE-Computing-Notes'],
-    'date_created': [date[-4:]],
-    'date_published': '2024',
-    "distribution": {
-        "formats": ["pdf"],
-        "number_files": 1,
-        "size": int(size)},
-    'experiment': ['JADE'],
-    'files': [{'checksum': 'adler32:'+ checksum,
-        'size': int(size),
-        'uri': 'root://eospublic.cern.ch/' + path}],
-    'license': {'attribution': 'CC0'},
-    'publisher': 'CERN Open Data Portal',
-    'recid': str(recid),
-    'title': 'JADE Computing ' + fields[0],
-    'type': {'primary': 'Supplementaries', 'secondary': ['Computing Note']},}
+    entry = {
+        "abstract": {"description": fields[1]},
+        "accelerator": "DESY-PETRA",
+        "collaboration": {"name": "JADE collaboration", "recid": "26050"},
+        "collections": ["JADE-Computing-Notes"],
+        "date_created": [date[-4:]],
+        "date_published": "2024",
+        "distribution": {"formats": ["pdf"], "number_files": 1, "size": int(size)},
+        "experiment": ["JADE"],
+        "files": [
+            {
+                "checksum": "adler32:" + checksum,
+                "size": int(size),
+                "uri": "root://eospublic.cern.ch/" + path,
+            }
+        ],
+        "license": {"attribution": "CC0"},
+        "publisher": "CERN Open Data Portal",
+        "recid": str(recid),
+        "title": "JADE Computing " + fields[0],
+        "type": {"primary": "Supplementaries", "secondary": ["Computing Note"]},
+    }
     if authors:
         formatauthor = []
         for author in authors.split(", "):
-            formatauthor.append({'name': str(author)})
-        entry['authors'] = formatauthor
+            formatauthor.append({"name": str(author)})
+        entry["authors"] = formatauthor
     entry = dict(sorted(entry.items()))
     cndict.append(entry)
     recid += 1
 
-with open('jade-computing-notes.json', 'w', encoding='utf-8') as f:
+with open("jade-computing-notes.json", "w", encoding="utf-8") as f:
     json.dump(cndict, f, ensure_ascii=False, indent=2)
